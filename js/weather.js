7302f798e89ee4f300ac83a530b0cbf9de3f4ca3
mungmongy <95222657+mungmongy@users.noreply.github.com>
2021-12-26 20:05:46 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ const API_KEY = "1e7010c03774da30e8f56666c70b6a39";
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    const url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
